0e2c59f3a55e892e27de362ef39ec94b5706f91d
Redempt <instantiable@gmail.com>
2021-12-18 17:54:47 -0500

    Fix docs so they update on newer releases

--- src/redempt/redmaven/DocsBuilder.java
+++ src/redempt/redmaven/DocsBuilder.java
@@ -18,21 +18,22 @@ public class DocsBuilder {
 		Path docsDir = getDocsDir(info);
 		long commitTime = getTime(gitDir, info.version());
 		if (!Files.exists(docsDir)) {
-			buildDocs0(buildDir, docsDir, commitTime);
+			buildDocs0(buildDir, docsDir, commitTime, info);
 			return;
 		}
 		Path timeFile = docsDir.resolve("time");
 		long time = Long.parseLong(Files.lines(timeFile).findFirst().get());
-		if (time > commitTime) {
-			buildDocs0(buildDir, docsDir, commitTime);
+		if (commitTime > time) {
+			buildDocs0(buildDir, docsDir, commitTime, info);
 		}
 	}
 	
-	private static void buildDocs0(Path buildDir, Path docsDir, long time) throws IOException {
+	private static void buildDocs0(Path buildDir, Path docsDir, long time, ProjectInfo info) throws IOException {
 		Optional<String> jarname = Files.list(buildDir).map(Path::getFileName).map(Path::toString).filter(s -> s.contains("javadoc") && s.endsWith(".jar")).findFirst();
 		if (jarname.isEmpty()) {
 			return;
 		}
+		System.out.println("Updating docs for " + info);
 		JarFile jar = new JarFile(buildDir.resolve(jarname.get()).toFile());
 		if (Files.exists(docsDir)) {
 			Utils.deleteRecursive(docsDir);
